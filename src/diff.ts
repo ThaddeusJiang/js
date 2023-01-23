4b2f103b20a02c6ba785312f3272866c0c2fcd35
TJ <thaddeusjiang@gmail.com>
2023-01-23 19:01:13 +0900

    feat: 🎸 diff

--- src/diff.ts
+++ src/diff.ts
@@ -1,19 +1,23 @@
 export const diff = (value: any[], origin: any[]) => {
-  const diff = value.map(item => {
-    const originItem = origin.find(i => i.id === item.id);
-    if (originItem) {
+  const diff = value
+    .map(item => {
+      const originItem = origin.find(i => i.id === item.id)
+      if (originItem) {
+        if (JSON.stringify(originItem) === JSON.stringify(item)) {
+          return null
+        } else {
+          return {
+            ...item,
+            method: 'update',
+          }
+        }
+      }
       return {
         ...item,
-        name: originItem.name === item.name ? undefined : item.name,
-      };
-    }
-    return {
-      ...item,
-      new: true,
-    };
-  });
-  const deleted = origin
-    .filter(item => !value.find(i => i.id === item.id))
-    .map(item => ({ ...item, deleted: true }));
-  return [...diff, ...deleted];
-};
+        method: 'create',
+      }
+    })
+    .filter(item => !!item)
+  const deletes = origin.filter(item => !value.find(i => i.id === item.id)).map(item => ({ ...item, method: 'delete' }))
+  return [...diff, ...deletes]
+}
--- test/diff.test.ts
+++ test/diff.test.ts
@@ -1,29 +1,51 @@
-import { diff } from '../src'
+import { diff } from '../src/diff'
+
 test('diff: should return the diff between two arrays', () => {
   const data = [
-    { id: 'org:2', name: 'ソリューション（旧）' },
-    { id: 'org:3', name: '新規開拓室' },
-    { id: 'org:4', name: '営業部' },
+    {
+      id: 'department:2',
+      name: 'ソリューション（旧）',
+    },
+    {
+      id: 'department:3',
+      name: '新規開拓室',
+    },
+    {
+      id: 'department:4',
+      name: '営業部',
+    },
   ]
 
   const origin = [
-    { id: 'org:1', name: 'SES' },
-    { id: 'org:2', name: 'ソリューション' },
-    { id: 'org:3', name: '新規開拓室' },
+    {
+      id: 'department:1',
+      name: 'SES',
+    },
+    {
+      id: 'department:2',
+      name: 'ソリューション',
+    },
+    {
+      id: 'department:3',
+      name: '新規開拓室',
+    },
   ]
 
   const diffData = [
-    { id: 'org:2', name: 'ソリューション（旧）' },
-    { id: 'org:3' },
     {
-      id: 'org:4',
-      name: '営業部',
-      new: true, // don't need this if DB client has upsert
+      id: 'department:2',
+      name: 'ソリューション（旧）',
+      method: 'update',
     },
     {
-      id: 'org:1',
+      id: 'department:4',
+      name: '営業部',
+      method: 'create',
+    },
+    {
+      id: 'department:1',
       name: 'SES',
-      deleted: true, // equal to deleted_at: new Date()
+      method: 'delete',
     },
   ]
 

ac4c88c620dd55113400de253c55819289121f30
TJ <thaddeusjiang@gmail.com>
2023-01-27 17:58:12 +0900

    test: 💍 use doctest-js
    I am on a day off today.

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+const config = {
+  testMatch: ['<rootDir>/**/*.(spec|test).{ts,tsx,js,jsx,mjs}'],
+}
+
+module.exports = config
--- src/math.ts
+++ src/math.ts
@@ -1,18 +1,34 @@
-// TODO: 这里如何可以像 ExDoc 一样生成文档，并且自动测试，就好了
 /**
  *
  * @param value
  * @param num
  * @returns
  *
- * examples:
- * ```js
- *  round(1.23456, 2) // 1.23
- *  round(1.23456, 4) // 1.2346
- * ```
+ * @example round(1.23456, 2)
+ * //=> 1.23
+ * @example round(1.23456, 4)
+ * //=> 1.2346
  */
 export const round = (value: number, num: number) => Math.round(value * Math.pow(10, num)) / Math.pow(10, num)
 
+/**
+ *
+ * @param value
+ * @param num
+ * @returns
+ *
+ * @example roundUp(1.23456, 2)
+ * //=> 1.24
+ */
 export const roundUp = (value: number, num: number) => Math.ceil(value * Math.pow(10, num)) / Math.pow(10, num)
 
+/**
+ *
+ * @param value
+ * @param num
+ * @returns
+ *
+ * @example roundDown(1.23456, 4)
+ * //=> 1.2345
+ */
 export const roundDown = (value: number, num: number) => Math.floor(value * Math.pow(10, num)) / Math.pow(10, num)
